72b0f08fed25adb58e9957c72e719d043e39ae87
aditya nikam <adityanikam05@gmail.com>
2024-01-17 14:57:46 +0530

    codeRefacter

---
+++ src/main/java/com/example/demo1/aditya.java
@@ -0,0 +1,6 @@
+package com.example.demo1;
+import org.springframework.web.bind.annotation.GetMapping;
+public class aditya {
+    @GetMapping("/aditya_airlines")
+    public String getData() { return "Please book your airlines ticket on aditya Airlines";}
+    }
